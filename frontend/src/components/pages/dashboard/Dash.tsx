bad54740b6eeb1f2d0af86d3c55899b4a391e840
Gowtham <gajavijay8@gmail.com>
2025-01-03 20:09:56 +0530

    Adding sidebar ans Dashboard

---
+++ frontend/src/components/pages/dashboard/Dash.tsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import { AppSidebar } from "./SideBar";
+import { SidebarProvider } from "@/components/ui/sidebar";
+
+export default function Dash() {
+  return (
+    <div className="w-full h-screen overflow-hidden">
+      <SidebarProvider>
+        <AppSidebar />
+
+        <Outlet />
+      </SidebarProvider>
+    </div>
+  );
+}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
 import Auth from "./components/pages/auth/Auth.tsx";
+import Dash from "./components/pages/dashboard/Dash.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -11,6 +12,9 @@ createRoot(document.getElementById("root")!).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/auth" element={<Auth />} />
+        <Route path="/dashboard" element={<Dash />}>
+          <Route path="" element={<Navigate to="analyticsall" replace />} />{" "}
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
